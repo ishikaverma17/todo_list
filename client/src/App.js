40d1d385e04588b6dbe5536be03a1d86b0d13aa7
Ishika verma <116638453+ishikaverma17@users.noreply.github.com>
2023-04-27 10:51:04 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import ToDo from "./components/ToDo";
+import { getAllToDo, addToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
+
+function App() {
+
+  const [toDo, setToDo] = useState([])
+  const [text,setText] = useState("")
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [toDoId, setToDoId] = useState("")
+
+
+  useEffect(() => {
+    getAllToDo(setToDo)
+  }, [])
+
+  const updateMode =(_id, text) => {
+    setIsUpdating(true)
+    setText(text)
+    setToDoId(_id)
+  } 
+
+  return (
+    <div className="App">
+     <div className="container">
+      <h1> 🖤!!...ToDo List...!!💜</h1>
+      <div className="top">
+        <input
+         type="text"
+          placeholder="Add ToDo...😀" 
+            value={text}
+            onChange={(e) => setText (e.target.value)}
+          />
+
+        <div className="add"
+         onClick={ isUpdating ?
+           () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+         : () => addToDo(text, setText, setToDo)}>
+        {isUpdating ? "Update....😉" : "Add...😍"}
+        </div>
+      </div>
+      <div className="list">
+
+      {toDo.map((item)=> <ToDo
+       key={item._id}
+        text={item.text}
+        updateMode = {() => updateMode(item._id, item.text)} 
+        deleteToDo = {() => deleteToDo(item._id, setToDo)} />)}
+
+      </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
